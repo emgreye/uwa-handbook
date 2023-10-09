fedab3f1c092706a18b7f609d4717fda358f2268
emgreye <JakeRMcAuliffe@gmail.com>
2023-10-09 14:54:41 +0800

    add more

--- buildgraph.py
+++ buildgraph.py
@@ -12,13 +12,22 @@ g.bind("ns", ns)
 with open("majors.json", encoding='utf-8') as file:
         m = json.load(file)
 
-title_of = URIRef(ns['title_of'])
-school_of = URIRef(ns['school_of'])
-board_of = URIRef(ns['board_of'])
-mode_of = URIRef(ns['mode_of'])
+with open("units.json", encoding='utf-8') as file:
+        u = json.load(file)
+
+has_school = URIRef(ns['has_school'])
+has_board = URIRef(ns['has_board'])
+has_mode = URIRef(ns['has_mode'])
 desc_of = URIRef(ns['desc_of'])
 outcome_of = URIRef(ns['outcome_of'])
-prereq_of = URIRef(ns['prereq_of'])
+has_prereq = URIRef(ns['has_prereq'])
+level_of = URIRef(ns['level_of'])
+credit_of = URIRef(ns['credit_of'])
+assessment_of = URIRef(ns['assessment_of'])
+advisable_of = URIRef(ns['advisable_of'])
+contacttype = URIRef(ns['contacttype'])
+contact_of = URIRef(ns['contact_of'])
+hours = URIRef(ns['hours'])
 unit = URIRef(ns['unit'])
 course = URIRef(ns['course'])
 major = URIRef(ns['major'])
@@ -31,27 +40,69 @@ for maj in m:
         g.add((imajor, RDF.type, major))
         g.add((imajor, FOAF.name, Literal(m[maj]['title'])))
         school = URIRef(ns[m[maj]['school'].replace(" ","_")])
-        g.add((imajor, school_of, school))
-        board = URIRef(ns[m[maj]['board_of_examiners'].replace(" ","_")])
-        g.add((imajor, board_of, board))
+        g.add((imajor, has_school, school))
+        board = URIRef(ns[m[maj]['has_board_examiners'].replace(" ","_")])
+        g.add((imajor, has_board, board))
         mode = URIRef(ns[m[maj]['delivery_mode'].replace(" ","_")])
-        g.add((imajor, mode_of, mode))
+        g.add((imajor, has_mode, mode))
         g.add((imajor, desc_of, Literal(m[maj]['description'])))
         g.add((imajor, outcome_of, Literal(m[maj]['outcomes'])))
         if ('prerequisites' in m[maj] ):
-                g.add((imajor, prereq_of, Literal(m[maj]['prerequisites'])))
+                g.add((imajor, has_prereq, Literal(m[maj]['prerequisites'])))
         for course in m[maj]['courses']:
                 prereq_course = URIRef(ns[course.replace(" ","_")])
-                g.add((imajor, prereq_of, prereq_course))
+                g.add((imajor, has_prereq, prereq_course))
                 g.add((prereq_course, RDF.type, RDFS.Class))
         for bridge in m[maj]['bridging']:
                 prereq_course = URIRef(ns[bridge.replace(" ","_")])
-                g.add((imajor, prereq_of, prereq_course))
+                g.add((imajor, has_prereq, prereq_course))
                 g.add((prereq_course, RDF.type, unit))
         for units in m[maj]['units']:
                 prereq_course = URIRef(ns[units.replace(" ","_")])
-                g.add((imajor, prereq_of, prereq_course))
+                g.add((imajor, has_prereq, prereq_course))
                 g.add((prereq_course, RDF.type, unit))
+        break
+
+for uniti in u:
+        iunit = URIRef((ns[u[uniti]['code'].replace(" ","_")]))
+        g.add((iunit, RDF.type, unit))
+        g.add((iunit, FOAF.name, Literal(u[uniti]['title'])))
+        school = URIRef(ns[u[uniti]['school'].replace(" ","_")])
+        g.add((iunit, has_school, school))
+        board = URIRef(ns[u[uniti]['has_board_examiners'].replace(" ","_")])
+        g.add((iunit, has_board, board))
+        mode = URIRef(ns[u[uniti]['delivery_mode'].replace(" ","_")])
+        g.add((iunit, has_mode, mode))
+        g.add((iunit, level_of, Literal(int(u[uniti]['level']))))
+        g.add((iunit, desc_of, Literal(u[uniti]['description'])))
+        g.add((iunit, credit_of, Literal(int(u[uniti]['credit']))))
+        if ('outcomes' in u[uniti] ):
+                g.add((iunit, outcome_of, Literal(u[uniti]['outcomes'])))
+        for assess in u[uniti]['assessment']:
+                g.add((iunit, assessment_of, Literal(assess)))
+        if ('prerequisites_text' in u[uniti] ):
+                g.add((iunit, has_prereq, Literal(u[uniti]['prerequisites_text'])))
+        if ('note' in u[uniti] ):
+                g.add((iunit, has_prereq, Literal(u[uniti]['note'])))
+        if ('text' in u[uniti] ):
+                g.add((iunit, has_prereq, Literal(u[uniti]['text'])))
+        if ('prerequisities_cnf' in u[uniti] ):
+                for lis in u[uniti]['prerequisites_cnf']:
+                        for un in lis:
+                                uni = URIRef(ns[un])
+                                g.add((iunit, has_prereq, uni))
+        if ('advisable_prior_study' in u[uniti] ):
+                for un in u[uniti]["advisable_prior_study"]:
+                        uni = URIRef(ns[un])
+                        g.add((iunit, advisable_of, uni))
+        if ('contact' in u[uniti] ):
+                for entry in u[uniti]['contact']:
+                        contact = BNode()
+                        g.add((iunit, contact_of, contact))
+                        g.add((contact, hours, Literal(int(u[uniti]['contact'][entry]))))
+                        g.add((contact, contacttype, Literal(entry)))
+        break
+
 
 print(g.serialize(format="turtle"))
 
