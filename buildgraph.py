fdc9de6dbf341f80bf56397b9857f5e732f870d9
emgreye <JakeRMcAuliffe@gmail.com>
2023-10-11 17:55:33 +0800

    sparql almost done

--- buildgraph.py
+++ buildgraph.py
@@ -1,6 +1,6 @@
 from rdflib import Graph, Literal, RDF, URIRef, BNode, RDFS, Namespace
 from rdflib.namespace import FOAF, RDFS, RDF
-from pyshacl import validate
+#from pyshacl import validate
 import json
 
 # Create a Graph
@@ -41,12 +41,13 @@ for maj in m:
         g.add((imajor, FOAF.name, Literal(m[maj]['title'])))
         school = URIRef(ns[m[maj]['school'].replace(" ","_")])
         g.add((imajor, has_school, school))
-        board = URIRef(ns[m[maj]['has_board_examiners'].replace(" ","_")])
+        board = URIRef(ns[m[maj]['board_of_examiners'].replace(" ","_")])
         g.add((imajor, has_board, board))
         mode = URIRef(ns[m[maj]['delivery_mode'].replace(" ","_")])
         g.add((imajor, has_mode, mode))
         g.add((imajor, desc, Literal(m[maj]['description'])))
-        g.add((imajor, has_outcome, Literal(m[maj]['outcomes'])))
+        for outcome in m[maj]['outcomes']:
+                g.add((imajor, has_outcome, Literal(outcome)))
         if ('prerequisites' in m[maj] ):
                 g.add((imajor, has_prereq, Literal(m[maj]['prerequisites'])))
         for course in m[maj]['courses']:
@@ -61,7 +62,6 @@ for maj in m:
                 prereq_course = URIRef(ns[units.replace(" ","_")])
                 g.add((imajor, has_prereq, prereq_course))
                 g.add((prereq_course, RDF.type, unit))
-        break
 
 for uniti in u:
         iunit = URIRef((ns[u[uniti]['code'].replace(" ","_")]))
@@ -69,7 +69,7 @@ for uniti in u:
         g.add((iunit, FOAF.name, Literal(u[uniti]['title'])))
         school = URIRef(ns[u[uniti]['school'].replace(" ","_")])
         g.add((iunit, has_school, school))
-        board = URIRef(ns[u[uniti]['has_board_examiners'].replace(" ","_")])
+        board = URIRef(ns[u[uniti]['board_of_examiners'].replace(" ","_")])
         g.add((iunit, has_board, board))
         mode = URIRef(ns[u[uniti]['delivery_mode'].replace(" ","_")])
         g.add((iunit, has_mode, mode))
@@ -77,7 +77,8 @@ for uniti in u:
         g.add((iunit, desc, Literal(u[uniti]['description'])))
         g.add((iunit, credit, Literal(int(u[uniti]['credit']))))
         if ('outcomes' in u[uniti] ):
-                g.add((iunit, has_outcome, Literal(u[uniti]['outcomes'])))
+                for outcome in u[uniti]['outcomes']:
+                        g.add((iunit, has_outcome, Literal(outcome)))
         for assess in u[uniti]['assessment']:
                 g.add((iunit, assessment_of, Literal(assess)))
         if ('prerequisites_text' in u[uniti] ):
@@ -91,6 +92,7 @@ for uniti in u:
                         for un in lis:
                                 uni = URIRef(ns[un])
                                 g.add((iunit, has_prereq, uni))
+                                g.add((uni, RDF.type, unit))
         if ('advisable_prior_study' in u[uniti] ):
                 for un in u[uniti]["advisable_prior_study"]:
                         uni = URIRef(ns[un])
@@ -101,10 +103,113 @@ for uniti in u:
                         g.add((iunit, has_contact, contact))
                         g.add((contact, hours, Literal(int(u[uniti]['contact'][entry]))))
                         g.add((contact, contacttype, Literal(entry)))
-        break
 
 
-print(g.serialize(format="turtle"))
+# print(g.serialize(format="turtle"))
+g.serialize(destination = 'handbook.ttl', format="ttl")
 
-# create nodes with x = URIref('URL')
-# or x = BNode()
+def pretty(uri): return uri.split("/")[-1]
+
+# SPARQL Queries
+# Find All Units with > 6 Outcomes.
+
+units_6_or_more_outcomes = """
+SELECT ?unit (COUNT(?o) AS ?c)
+WHERE {
+        ?unit ns:has_outcome ?o .
+}
+GROUP BY ?unit
+HAVING (COUNT(?o) > 6)
+"""
+# for row in g.query(units_6_or_more_outcomes):
+#         print(f"{row.unit} has {row.c} outcomes")
+
+level_3_unit_no_exam_no_pre_req_exam = """
+SELECT ?unit
+WHERE {
+        ?unit ns:level 3 .
+        FILTER NOT EXISTS {
+                SELECT ?unit ?assess
+                WHERE {
+                        ?unit ns:assessment_of ?assess .
+                        FILTER (CONTAINS(?assess, "exam"))
+                } 
+        }
+        OPTIONAL {
+        ?unit ns:has_prereq ?ppp . 
+				FILTER NOT EXISTS {
+                SELECT ?unit ?pre ?p_assess
+                WHERE {
+                        ?unit ns:has_prereq ?pre .
+                        ?pre ns:assessment_of ?p_assess .
+                        FILTER (CONTAINS(?p_assess, "exam"))
+                } 
+        }
+}
+
+"""
+
+level_3_not_quite = """
+SELECT ?unit
+WHERE {
+        ?unit ns:level 3 .
+        FILTER NOT EXISTS {
+                SELECT ?unit ?assess
+                WHERE {
+                        ?unit ns:assessment_of ?assess .
+                        FILTER (CONTAINS(?assess, "exam"))
+                } 
+        }
+}
+
+"""
+
+test = """
+SELECT ?unit ?pre
+		WHERE {
+                ?unit rdf:type ns:unit .
+				?unit ns:has_prereq ?pre .
+				?pre rdf:type ns:unit .
+		}
+"""
+#the above doesn't work because some pre-requisites are not units
+
+more_3_maj = """
+SELECT ?unit (COUNT(?maj) AS ?c)
+WHERE {
+        ?maj ns:has_prereq ?unit .
+        ?unit rdf:type ns:unit .
+} GROUP BY ?unit
+HAVING (COUNT(?maj) > 3)
+"""
+
+contains_env = """
+SELECT ?unit
+WHERE {
+        ?unit ns:desc ?descr .
+        ?unit ns:has_outcome ?out .
+        ?unit rdf:type ns:unit .
+        FILTER (CONTAINS(?descr, "environmental policy") || CONTAINS(?out, "environmental policy"))
+} GROUP BY ?unit
+"""
+
+contains_keyword = """
+SELECT ?unit ?value
+WHERE {
+        ?unit ns:desc ?descr .
+        ?unit ns:has_outcome ?out .
+        ?unit rdf:type ns:unit .
+        FILTER (CONTAINS(?descr, "word") || CONTAINS(?out, "word"))
+} GROUP BY ?unit
+"""
+
+for row in g.query(level_3_not_quite):
+        print(f"{row.unit} has no exams")
+
+#for row in g.query(more_3_maj):
+#        print(f"{row.unit} is in {row.c} majors")
+
+value = "people are"
+
+for row in g.query(contains_keyword.replace("word", value)):
+        print(f"{row.unit} contains the phrase {value}")
