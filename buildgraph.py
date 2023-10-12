3b769f4d1f2fea2cc8ef0c77ad9b8bf5becc246d
emgreye <JakeRMcAuliffe@gmail.com>
2023-10-12 15:17:34 +0800

    update

--- buildgraph.py
+++ buildgraph.py
@@ -1,7 +1,115 @@
 from rdflib import Graph, Literal, RDF, URIRef, BNode, RDFS, Namespace
 from rdflib.namespace import FOAF, RDFS, RDF
-#from pyshacl import validate
+from pyshacl import validate
 import json
+
+def query1(graph):
+	units_6_or_more_outcomes = """
+	SELECT ?unit (COUNT(?o) AS ?c)
+	WHERE {
+			?unit ns:has_outcome ?o .
+	}
+	GROUP BY ?unit
+	HAVING (COUNT(?o) > 6)
+	"""
+	for row in graph.query(units_6_or_more_outcomes):
+			print(f"{row.unit} has {row.c} outcomes")
+                        
+def query2(graph):
+	level_3_unit_no_exam_no_pre_req_exam = """
+	SELECT ?unit
+	WHERE {
+			?unit ns:level 3 .
+			FILTER NOT EXISTS {
+					SELECT ?unit ?assess
+					WHERE {
+							?unit ns:assessment_of ?assess .
+							FILTER (CONTAINS(?assess, "exam"))
+					} 
+			}
+			OPTIONAL {
+			?unit ns:has_prereq ?ppp . 
+					FILTER NOT EXISTS {
+					SELECT ?unit ?pre ?p_assess
+					WHERE {
+							?unit ns:has_prereq ?pre .
+							?pre ns:assessment_of ?p_assess .
+							FILTER (CONTAINS(?p_assess, "exam"))
+					} 
+			}
+	}
+
+	"""
+
+	level_3_not_quite = """
+	SELECT ?unit
+	WHERE {
+			?unit ns:level 3 .
+			FILTER NOT EXISTS {
+					SELECT ?unit ?assess
+					WHERE {
+							?unit ns:assessment_of ?assess .
+							FILTER (CONTAINS(?assess, "exam"))
+					} 
+			}
+	}
+
+	"""
+
+	test = """
+	SELECT ?unit ?pre
+			WHERE {
+					?unit rdf:type ns:unit .
+					?unit ns:has_prereq ?pre .
+					?pre rdf:type ns:unit .
+			}
+	"""
+	#the above doesn't work because some pre-requisites are not units
+
+	for row in graph.query(level_3_not_quite):
+		print(f"{row.unit} has no exams")
+                
+def query3(graph):
+	more_3_maj = """
+	SELECT ?unit (COUNT(?maj) AS ?c)
+	WHERE {
+			?maj ns:has_unit ?unit .
+			?unit rdf:type ns:unit .
+	} GROUP BY ?unit
+	HAVING (COUNT(?maj) > 3)
+	"""
+	for row in graph.query(more_3_maj):
+		print(f"{row.unit} is a unit in more than three majors")
+                
+def query4(word, graph):
+	contains_env = """
+	SELECT ?unit
+	WHERE {
+			?unit ns:desc ?descr .
+			?unit ns:has_outcome ?out .
+			?unit rdf:type ns:unit .
+			FILTER (CONTAINS(?descr, "environmental policy") || CONTAINS(?out, "environmental policy"))
+	} GROUP BY ?unit
+	"""
+
+	contains_keyword = """
+	SELECT ?unit ?value
+	WHERE {
+			?unit ns:desc ?descr .
+			?unit ns:has_outcome ?out .
+			?unit rdf:type ns:unit .
+			FILTER (CONTAINS(?descr, "word") || CONTAINS(?out, "word"))
+	} GROUP BY ?unit
+	"""
+
+
+	#for row in g.query(more_3_maj):
+	#        print(f"{row.unit} is in {row.c} majors")
+
+	value = word
+
+	for row in graph.query(contains_keyword.replace("word", value)):
+			print(f"{row.unit} contains the phrase '{value}'.")
 
 # Create a Graph
 g = Graph()
@@ -21,6 +129,8 @@ has_mode = URIRef(ns['has_mode'])
 desc = URIRef(ns['desc'])
 has_outcome = URIRef(ns['has_outcome'])
 has_prereq = URIRef(ns['has_prereq'])
+has_bridge = URIRef(ns['has_bridge'])
+has_unit = URIRef(ns['has_unit'])
 level = URIRef(ns['level'])
 credit = URIRef(ns['credit'])
 assessment_of = URIRef(ns['assessment_of'])
@@ -56,11 +166,11 @@ for maj in m:
                 g.add((prereq_course, RDF.type, RDFS.Class))
         for bridge in m[maj]['bridging']:
                 prereq_course = URIRef(ns[bridge.replace(" ","_")])
-                g.add((imajor, has_prereq, prereq_course))
+                g.add((imajor, has_bridge, prereq_course))
                 g.add((prereq_course, RDF.type, unit))
         for units in m[maj]['units']:
                 prereq_course = URIRef(ns[units.replace(" ","_")])
-                g.add((imajor, has_prereq, prereq_course))
+                g.add((imajor, has_unit, prereq_course))
                 g.add((prereq_course, RDF.type, unit))
 
 for uniti in u:
@@ -110,106 +220,14 @@ g.serialize(destination = 'handbook.ttl', format="ttl")
 
 def pretty(uri): return uri.split("/")[-1]
 
-# SPARQL Queries
-# Find All Units with > 6 Outcomes.
+sg = Graph()
+with open("shaclconstraints.ttl") as f:
+    sg.parse(data=f.read(), format='ttl')
 
-units_6_or_more_outcomes = """
-SELECT ?unit (COUNT(?o) AS ?c)
-WHERE {
-        ?unit ns:has_outcome ?o .
-}
-GROUP BY ?unit
-HAVING (COUNT(?o) > 6)
-"""
-# for row in g.query(units_6_or_more_outcomes):
-#         print(f"{row.unit} has {row.c} outcomes")
-
-level_3_unit_no_exam_no_pre_req_exam = """
-SELECT ?unit
-WHERE {
-        ?unit ns:level 3 .
-        FILTER NOT EXISTS {
-                SELECT ?unit ?assess
-                WHERE {
-                        ?unit ns:assessment_of ?assess .
-                        FILTER (CONTAINS(?assess, "exam"))
-                } 
-        }
-        OPTIONAL {
-        ?unit ns:has_prereq ?ppp . 
-				FILTER NOT EXISTS {
-                SELECT ?unit ?pre ?p_assess
-                WHERE {
-                        ?unit ns:has_prereq ?pre .
-                        ?pre ns:assessment_of ?p_assess .
-                        FILTER (CONTAINS(?p_assess, "exam"))
-                } 
-        }
-}
-
-"""
-
-level_3_not_quite = """
-SELECT ?unit
-WHERE {
-        ?unit ns:level 3 .
-        FILTER NOT EXISTS {
-                SELECT ?unit ?assess
-                WHERE {
-                        ?unit ns:assessment_of ?assess .
-                        FILTER (CONTAINS(?assess, "exam"))
-                } 
-        }
-}
-
-"""
-
-test = """
-SELECT ?unit ?pre
-		WHERE {
-                ?unit rdf:type ns:unit .
-				?unit ns:has_prereq ?pre .
-				?pre rdf:type ns:unit .
-		}
-"""
-#the above doesn't work because some pre-requisites are not units
-
-more_3_maj = """
-SELECT ?unit (COUNT(?maj) AS ?c)
-WHERE {
-        ?maj ns:has_prereq ?unit .
-        ?unit rdf:type ns:unit .
-} GROUP BY ?unit
-HAVING (COUNT(?maj) > 3)
-"""
-
-contains_env = """
-SELECT ?unit
-WHERE {
-        ?unit ns:desc ?descr .
-        ?unit ns:has_outcome ?out .
-        ?unit rdf:type ns:unit .
-        FILTER (CONTAINS(?descr, "environmental policy") || CONTAINS(?out, "environmental policy"))
-} GROUP BY ?unit
-"""
-
-contains_keyword = """
-SELECT ?unit ?value
-WHERE {
-        ?unit ns:desc ?descr .
-        ?unit ns:has_outcome ?out .
-        ?unit rdf:type ns:unit .
-        FILTER (CONTAINS(?descr, "word") || CONTAINS(?out, "word"))
-} GROUP BY ?unit
-"""
-
-for row in g.query(level_3_not_quite):
-        print(f"{row.unit} has no exams")
-
-#for row in g.query(more_3_maj):
-#        print(f"{row.unit} is in {row.c} majors")
-
-value = "people are"
-
-for row in g.query(contains_keyword.replace("word", value)):
-        print(f"{row.unit} contains the phrase {value}")
+results = validate(
+g,
+sg=sg,
+inference='both'
+)
+(conforms, results_graph, results_text) = results
+print(results_text)
